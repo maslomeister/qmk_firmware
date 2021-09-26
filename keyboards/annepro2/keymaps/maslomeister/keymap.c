9a845155c2f4d4e3d641c0c3c00daa4292eee739
Maslomeister <maslomeister@gmail.com>
2021-09-26 13:13:49 +0300

    small fixes

--- keyboards/annepro2/keymaps/maslomeister/keymap.c
+++ keyboards/annepro2/keymaps/maslomeister/keymap.c
@@ -49,11 +49,11 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------------------------------------.
      * | Esc |  F1 |  F2 |  F3 |  F4 |  F5 |  F6 |  F7 |  F8 |  F9 | F10 | F11 | F12 |  DELETE   |
      * |-----------------------------------------------------------------------------------------+
-     * |         |PGDN| UP  |PGUP |    |   |    |    |    |    | PS | HOME | END |               |
+     * |         |  HOME  | UP  | END |    |   |    |    |    |    |    |      |     |           |
      * |-----------------------------------------------------------------------------------------+
-     * |         |LEFT |DOWN |RIGHT|    |    |    |    |    |    |     |       |                 |
+     * |         |LEFT |DOWN |RIGHT|    |    | DEL|    |    |    |     |       |                 |
      * |-----------------------------------------------------------------------------------------+
-     * |    SHIFT   |V-UP |V-DWN|MUTE |     |     |     |     |     |INSRT| DEL |      UP        |
+     * |    SHIFT   |     |    |      |     |     |     |     |     |     |     |      UP        |
      * |-----------------------------------------------------------------------------------------+
      * |       |      |        |              space           |  Alt  |  LEFT  |  DOWN  | RIGHT  |
      * \-----------------------------------------------------------------------------------------/
@@ -61,9 +61,9 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_FN1_LAYER] = KEYMAP(/* Base */
     KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
-    KC_NO, KC_PGDN, KC_UP, KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_HOME, KC_END, KC_NO,
-    KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_NO, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_LSHIFT, KC_VOLU, KC_VOLD, KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_INS, KC_DEL, KC_UP,
+    KC_NO, KC_HOME, KC_UP, KC_END, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NO, KC_DEL, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_LSHIFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_UP, KC_UP, KC_UP,
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
     /*
@@ -71,11 +71,11 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------------------------------------------------------.
      * |     |     |     |    |    |   |   |   |LEDOF|LEDON|LED_INTENS|LED_SPEED|   |CTRL_ALT_DEL|
      * |-----------------------------------------------------------------------------------------+
-     * |        | KC_1| KC_2| KC_3|     |     |     |     |     |     | PS | HOME | END |        |
+     * |      | KC_1| KC_2| KC_3|  |  |PREV_TRACK|PLAY/PAUSE|NEXT_TRACK|   |   |    |     |      |
      * |-----------------------------------------------------------------------------------------+
-     * |          |KC_4 |KC_5 |KC_6|     |     |     |     |     |     |     |     |             |
+     * |          |KC_4 |KC_5 |KC_6|     |     |V_DOWN| MUTE|V_UP|     |     |     |             |
      * |-----------------------------------------------------------------------------------------+
-     * |            |  KC_7  |  KC_8  |  KC_9  |     |     |     |     |     |INSRT| DEL |       |
+     * |            |  KC_7  |  KC_8  |  KC_9  |     |     |     |     |     |      |    |       |
      * |-----------------------------------------------------------------------------------------+
      * |       |       |       |               KC_0             |       |       |  FN2  |        |
      * \-----------------------------------------------------------------------------------------/
@@ -83,9 +83,9 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_FN2_LAYER] = KEYMAP(/* Base */
     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_AP_LED_OFF, KC_AP_LED_ON, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_SPEED, KC_NO, LALT(LCTL(KC_DEL)),
-    KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_HOME, KC_END, KC_NO,
-    KC_NO, KC_4, KC_5, KC_6, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-    KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_INS, KC_DEL, KC_NO,
+    KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_NO, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_4, KC_5, KC_6, KC_NO, KC_NO, KC__VOLDOWN, KC__MUTE, KC__VOLUP, KC_NO, KC_NO, KC_NO, KC_NO,
+    KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
     KC_NO, KC_NO, KC_NO, KC_0, KC_NO, KC_NO, MO(_FN2_LAYER), KC_NO
     ),
 };
@@ -95,12 +95,13 @@ void enableProfileColor(uint8_t * profile, const uint16_t * keymap);
 void resetProfileColor(void);
 
 bool is_caps_set = false;
+uint8_t default_profile = 0;
 
-uint8_t base_profile[] = {0x66, 0x00, 0xFF};
+uint8_t base_profile[] = {0xC8, 0x00, 0xFF};
 uint8_t empty_profile[] = {0x00, 0x00, 0x00};
 uint8_t caps_profile[] = {0xFF,0x00,0x00};
 uint8_t fn1_profile[] = {0xFF,0x60,0xDF};
-uint8_t fn2_profile[] = {0x60,0xFF,0xFF};
+uint8_t fn2_profile[] = {0x60,0xFF,0xD5};
 
 void matrix_init_user(void) {}
 
@@ -174,7 +175,32 @@ void enableProfileColor (uint8_t * profile, const uint16_t * keymap) {
 void resetProfileColor(void) {
     if(is_caps_set) {
         annepro2LedSetForegroundColor(caps_profile[0], caps_profile[1], caps_profile[2]);
-    } else {
+    } else if (default_profile == 0) {
         annepro2LedSetForegroundColor(base_profile[0], base_profile[1], base_profile[2]);
+    } else {
+        annepro2LedSetProfile(default_profile);
     }
 }
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case KC_AP_LED_ON:
+        if (record->event.pressed) {
+            default_profile++;
+            if(default_profile == 16){
+                default_profile = 0;
+            }
+
+            if(default_profile == 0) {
+                annepro2LedSetForegroundColor(base_profile[0], base_profile[1], base_profile[2]);
+            } else {
+                annepro2LedSetProfile(default_profile);
+            }
+        }
+        return true;
+    default:
+        return true;
+    }
+}
+
+
