40dc6048ca7b53d4829b01fc27802671a592a75f
Maslomeister <maslomeister@gmail.com>
2021-09-26 23:01:37 +0300

    fix

--- keyboards/annepro2/keymaps/maslomeister/keymap.c
+++ keyboards/annepro2/keymaps/maslomeister/keymap.c
@@ -117,6 +117,7 @@ void matrix_scan_user(void) {
 
 void housekeeping_task_user(void){
     if(annepro2LedStatus.matrixEnabled && timer_elapsed32(key_timer) >= 10000){
+        print("keyboard was inactive for > 10 secs, led disable");
         annepro2LedDisable();
     }
 }
@@ -198,10 +199,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         if(annepro2LedStatus.matrixEnabled){
             key_timer = timer_read32();
+            print("button pressed, timer started\n");
         }else{
+            print("button pressed, led enabled\n");
             annepro2LedEnable();
         }
     }
     return true;
 }
-
